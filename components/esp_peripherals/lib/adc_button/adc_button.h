1bb0273805dc636b2f66c9c01ccf88f8d31830dc
dwit <dwitgen+github@gmail.com>
2025-02-10 18:04:18 -0500

    Update adc button init

--- components/esp_peripherals/lib/adc_button/adc_button.h
+++ components/esp_peripherals/lib/adc_button/adc_button.h
@@ -91,7 +91,7 @@ esp_err_t adc_button_adc_init(adc_channel_t channel);
 
 void adc_btn_delete_task(void);
 
-void button_task(void *parameters);
+static void button_task(void *parameters);
 
 #ifdef __cplusplus
 }

0e98a8f27b4114808d68ebdc7c8cc833f2785e07
dwit <dwitgen+github@gmail.com>
2025-02-14 14:14:46 -0500

    Update adc button init

--- components/esp_peripherals/lib/adc_button/adc_button.c
+++ components/esp_peripherals/lib/adc_button/adc_button.c
@@ -337,4 +337,5 @@ int get_adc_voltage(int channel) {
                         task_cfg->task_core);
     
     ESP_LOGE(TAG, "Button Task running on core: %d", xPortGetCoreID());
+    ESP_LOGE(TAG, "Button Task Handle: %p", tag->audio_thread);
 }
